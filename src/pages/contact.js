6f03c69248d4a59a8c233e0f7a439e00f9541424
Alex Belyeu <abelyeun@gmail.com>
2020-04-27 18:55:38 -0400

    Fix weird layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,16 @@ import SEO from "../components/seo"
 const Contact = () => (
   <LayoutPortfolio>
     <SEO title="Say hi!" />
-    <h1>Here you can contact me</h1>
-    <Link to="/">Go back to the homepage</Link>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0 1.0875rem 1.45rem`,
+      }}
+    >
+      <h1>Here you can contact me</h1>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
   </LayoutPortfolio>
 )
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import netflixData from '../data/netflix_shows.json';
+import netflixData from "../data/netflix_shows.json"
 
-import ScatterPlot from "../components/ScatterPlot";
-import Histogram from "../components/Histogram";
+import ScatterPlot from "../components/ScatterPlot"
+import Histogram from "../components/Histogram"
 import LayoutPortfolio from "../components/LayoutPortfolio"
 import SEO from "../components/seo"
 
@@ -12,14 +12,22 @@ const filteredData = netflixData.filter(d => !isNaN(d.userRatingScore))
 const Projects = () => (
   <LayoutPortfolio>
     <SEO title="Projects" />
-    <h1>Here go the projects</h1>
-    <div className="my-4 card" key={0}>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0 1.0875rem 1.45rem`,
+      }}
+    >
+      <h1>Here go the projects</h1>
+      <div className="my-4 card" key={0}>
         <ScatterPlot id={0} width={950} dataset={filteredData} />
       </div>
       <div className="card" key={1}>
         <Histogram id={1} width={950} dataset={filteredData} />
       </div>
-    <Link to="/">Go back to the homepage</Link>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
   </LayoutPortfolio>
 )
 
