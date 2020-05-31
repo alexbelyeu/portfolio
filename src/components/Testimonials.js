8fb1dc4477581672e25a5537aebb953059fa1ed7
Alex Belyeu <abelyeun@gmail.com>
2020-05-31 13:50:50 -0400

    (AB) Swipeable testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,11 +3,26 @@ import { testimonials } from "../data/testimonials"
 import { Card } from "./Card"
 
 const Testimonials = () => {
+  let x0
   const [viewable, setViewable] = useState(0)
   useEffect(() => {
     let root = document.documentElement
     root.style.setProperty("--viewable", viewable)
   }, [viewable])
+
+  const swipe = (start, end) => {
+    if (Math.abs(start - end) > 100) {
+      // Swipe right to go left
+      if (Math.sign(start - end) === -1) {
+        if (viewable === 0) return // Leftmost so nothing
+        setViewable(viewable - 1)
+      } else {
+        // Swipe left to go right
+        if (viewable === testimonials.length - 1) return // Rightmost so nothing
+        setViewable(viewable + 1)
+      }
+    }
+  }
 
   return (
     <div
@@ -19,6 +34,10 @@ const Testimonials = () => {
       </p>
       <div
         id="Carousel"
+        onTouchStart={e => (x0 = e.changedTouches[0].clientX)}
+        onTouchEnd={e => swipe(x0, e.changedTouches[0].clientX)}
+        onMouseDown={e => (x0 = e.clientX)}
+        onMouseUp={e => swipe(x0, e.clientX)}
         className="flex max-w-xs md:max-w-lg overflow-hidden"
       >
         {testimonials.map((testimonial, index) => (
@@ -40,8 +59,8 @@ const Testimonials = () => {
             id={`${testimonial.person}Dot`}
             className={`${index !== 0 && "ml-2"} ${
               index === viewable && "font-bold text-indigo-900"
-            }`}
-            onClick={e => {
+            } ${index !== viewable && "cursor-pointer"}`}
+            onClick={() => {
               setViewable(index)
             }}
           >
