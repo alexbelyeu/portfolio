05ed91fb2d244b3a6455bbe3d018483694417b9a
Alex Belyeu <abelyeun@gmail.com>
2020-07-05 12:19:18 -0400

    animations for menu buttons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,89 +1,104 @@
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
-import { animated, useTransition } from "react-spring"
+import { animated, useTrail } from "react-spring"
+
+const items = [
+  <a
+    href="/projects"
+    className="px-2 h-full flex justify-center items-center bg-indigo-300 border border-indigo-900 shadow-lg rounded-full hover:bg-indigo-200"
+  >
+    Netflix Study
+  </a>,
+  <a
+    href=""
+    className="px-2 h-full flex justify-center items-center bg-indigo-300 border border-indigo-900 shadow-lg rounded-full hover:bg-indigo-200"
+  >
+    Shooter Coaster 3D
+    <div className="w-6 flex justify-center items-center">
+      <FontAwesomeIcon
+        icon={faExternalLinkAlt}
+        className="text-indigo-900 absolute text-xs"
+      />
+    </div>
+  </a>,
+  <a
+    href=""
+    className="px-2 h-full flex justify-center items-center bg-indigo-300 border border-indigo-900 shadow-lg rounded-full hover:bg-indigo-200"
+  >
+    Somryst
+    <div className="w-6 flex justify-center items-center">
+      <FontAwesomeIcon
+        icon={faExternalLinkAlt}
+        className="text-indigo-900 absolute text-xs"
+      />
+    </div>
+  </a>,
+  <a
+    href=""
+    className="px-2 h-full flex justify-center items-center bg-indigo-300 border border-indigo-900 shadow-lg rounded-full hover:bg-indigo-200"
+  >
+    Sweetspot
+    <div className="w-6 flex justify-center items-center">
+      <FontAwesomeIcon
+        icon={faExternalLinkAlt}
+        className="text-indigo-900 absolute text-xs"
+      />
+    </div>
+  </a>,
+  <a
+    href=""
+    className="px-2 h-full flex justify-center items-center bg-indigo-300 border border-indigo-900 shadow-lg rounded-full hover:bg-indigo-200"
+  >
+    Slack Jukebox
+    <div className="w-6 flex justify-center items-center">
+      <FontAwesomeIcon
+        icon={faExternalLinkAlt}
+        className="text-indigo-900 absolute text-xs"
+      />
+    </div>
+  </a>,
+  <a
+    href=""
+    className="px-2 h-full flex justify-center items-center bg-indigo-300 border border-indigo-900 shadow-lg rounded-full hover:bg-indigo-200"
+  >
+    Tapjoy
+    <div className="w-6 flex justify-center items-center">
+      <FontAwesomeIcon
+        icon={faExternalLinkAlt}
+        className="text-indigo-900 absolute text-xs"
+      />
+    </div>
+  </a>,
+]
 
 const Menu = ({ showMenu }) => {
-  const transitions = useTransition(showMenu, null, {
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
+  const config = { mass: 5, tension: 4000, friction: 300 }
+  const trail = useTrail(items.length, {
+    config,
+    opacity: showMenu ? 1 : 0,
+    x: showMenu ? 0 : 20,
+    height: showMenu ? 40 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
   })
-  return transitions.map(({ item, key, props }) =>
-    item ? (
-      <animated.div
-        key={key}
-        style={props}
-        className="absolute bg-indigo-300 mt-1 flex flex-col justify-around w-48 h-48 rounded-sm shadow-xl"
-      >
-        <a
-          href="/projects"
-          className="px-2 h-full flex justify-between items-center hover:bg-indigo-200 rounded-t-sm"
+
+  return (
+    <div className="absolute mt-1 font-medium text-sm">
+      {trail.map(({ x, height, ...rest }, index) => (
+        <animated.div
+          key={index}
+          className="my-1 w-48"
+          style={{
+            ...rest,
+            transform: x.interpolate(
+              x => `translate3d(${index % 2 ? 20 : 0}px,${x}px,0)`
+            ),
+          }}
         >
-          Netflix Study
-        </a>
-        <a
-          href=""
-          className="px-2 h-full flex justify-between items-center border-t border-indigo-200 hover:bg-indigo-200"
-        >
-          Shooter Coaster 3D
-          <div className="w-5 h-5 flex justify-center items-center">
-            <FontAwesomeIcon
-              icon={faExternalLinkAlt}
-              className="text-indigo-900 absolute text-sm"
-            />
-          </div>
-        </a>
-        <a
-          href=""
-          className="px-2 h-full flex justify-between items-center border-t border-indigo-200 hover:bg-indigo-200"
-        >
-          Somryst
-          <div className="w-5 h-5 flex justify-center items-center">
-            <FontAwesomeIcon
-              icon={faExternalLinkAlt}
-              className="text-indigo-900 absolute text-sm"
-            />
-          </div>
-        </a>
-        <a
-          href=""
-          className="px-2 h-full flex justify-between items-center border-t border-indigo-200 hover:bg-indigo-200"
-        >
-          Sweetspot
-          <div className="w-5 h-5 flex justify-center items-center">
-            <FontAwesomeIcon
-              icon={faExternalLinkAlt}
-              className="text-indigo-900 absolute text-sm"
-            />
-          </div>
-        </a>
-        <a
-          href=""
-          className="px-2 h-full flex justify-between items-center border-t border-indigo-200 hover:bg-indigo-200"
-        >
-          Slack Jukebox
-          <div className="w-5 h-5 flex justify-center items-center">
-            <FontAwesomeIcon
-              icon={faExternalLinkAlt}
-              className="text-indigo-900 absolute text-sm"
-            />
-          </div>
-        </a>
-        <a
-          href=""
-          className="px-2 h-full flex justify-between items-center border-t border-indigo-200 hover:bg-indigo-200 rounded-b-sm"
-        >
-          Tapjoy
-          <div className="w-5 h-5 flex justify-center items-center">
-            <FontAwesomeIcon
-              icon={faExternalLinkAlt}
-              className="text-indigo-900 absolute text-sm"
-            />
-          </div>
-        </a>
-      </animated.div>
-    ) : null
+          <animated.div style={{ height }}>{items[index]}</animated.div>
+        </animated.div>
+      ))}
+    </div>
   )
 }
 
